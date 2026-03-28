fc5f23ec7e948dc9e5bb77e06e8be76928823ae8
Umar Abdulkabir <ukabir902@gmail.com>
2026-03-28 14:52:35 +0100

    fixed middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,13 @@
 // middleware.js
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+// Explicitly exporting the withAuth function satisfies the build requirement
+export default withAuth({
+  callbacks: {
+    // This simply ensures the user has a valid token (is logged in)
+    authorized: ({ token }) => !!token, 
+  },
+});
 
 export const config = {
   matcher: ["/admin/:path*"], // This protects the entire admin folder
