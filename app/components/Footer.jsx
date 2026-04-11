57edf3c37e7be3e198de71f8403b1926162c9a10
Umar Abdulkabir <ukabir902@gmail.com>
2026-04-11 16:44:36 +0100

    added insta link

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -30,7 +30,7 @@ export default function Footer() {
     },
     {
       platform: 'Instagram',
-      href: '#',
+      href: 'https://www.instagram.com/official_buharioyonorth/',
       // Instagram Icon
       icon: (
         <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -29,7 +29,7 @@ export default function ContactPage() {
     },
     {
       platform: 'Instagram',
-      href: '#',
+      href: 'https://www.instagram.com/official_buharioyonorth/',
       icon: (
         <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
           <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
